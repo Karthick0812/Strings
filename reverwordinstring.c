0d003b926cca7c883618e6d9c731afa0f043a2e3
Karthick0812 <144112326+Karthick0812@users.noreply.github.com>
2025-07-04 18:40:15 +0530

    Update reverwordinstring.c

--- reverwordinstring.c
+++ reverwordinstring.c
@@ -22,42 +22,7 @@ int main()
             start=i+1;
         }
     }
-  /* for(i=start,j=size-1;i<j;i++,j--)
-    {
-        temp=a[i];
-        a[i]=a[j];
-        a[j]=temp;
-    }*/
     printf("%s\n",a);
 }
-/*#include<stdio.h>
-int main()
-{
-    char a[100];
-    int i,start=0,end;
-    char temp;
-    
-    printf("Enter the string\n");
-    scanf("%[^\n]",a);
-    
-    for(i=0;a[i]!='\0';i++)
-    {
-        if((a[i]==' ')||(a[i+1]=='\0'))
-        {
-            if(a[i+1]=='\0'&&a[i]!=' ')
-            end=i;
-            else
-            end=i-1;
-            
-            int s,e;
-            for(s=start,e=end;s<e;s++,e--)
-            {
-                 temp=a[s];
-                 a[s]=a[e];
-                 a[e]=temp;
-             }
-                   start=i+1;
-        }
-    }
-    printf("%s",a);
-}*/
+  
+   
